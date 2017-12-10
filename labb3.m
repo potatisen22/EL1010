6e17b94f4e1ed6bffa2946033a019b19b0e3c16b
kimster96 <34395381+kimster96@users.noreply.github.com>
2017-12-10 15:53:56 +0100

    test

--- labb3.m
+++ labb3.m
@@ -43,23 +43,54 @@ G0 = K * G;
 %% 3.3
 clc
 close all
-bode(G)
+clear all
+[J,umax] = lab3robot(960703);
 s = tf('s')
-[Gm, Pm, Wbredd, Wcross] = margin(G)
-beta = 0.7;
-Wcd = 0.95;
+kt=38;
+Lm=2;
+km=0.5;
+n=1/20;
+Rm=21;
+b=1;
+Kg = kt/(s*Lm+Rm)
+T=1/(J*s+b)
+G = (Kg*T*n/s)/(km*Kg*T+1)
+figure()
+bode(G)
+Gc = G/(1+G)
+S11=stepinfo(Gc)
 
+[GGm, GPm, GWbredd, GWcross] = margin(G)
+beta = 0.8;
+Wcd = GWcross*2;
+K = 1;
 Td = 1/(Wcd*sqrt(beta));
-
-Flead = K*(Td*s+1)/(beta*Td+1);
-
-%F = Flead*Flag;
-Go = Flead*G;
+Ti = 10/Wcd;
+gamma = 0;
+s = i*Wcd;
+Kg = kt/(s*Lm+Rm)
+T=1/(J*s+b)
+G = (Kg*T*n/s)/(km*Kg*T+1)
+Flead = K*(Td*s+1)/(beta*Td*s+1);
+Flag = (Ti*s+1)/(Ti*s+gamma);
+F = Flead*Flag;
+K = 1/10^(abs(F*G)/20);
+s = tf('s');
+Kg = kt/(s*Lm+Rm)
+T=1/(J*s+b)
+G = (Kg*T*n/s)/(km*Kg*T+1)
+Flead = K*(Td*s+1)/(beta*Td*s+1);
+Flag = (Ti*s+1)/(Ti*s+gamma);
+F = Flead*Flag;
+Go = F*G;
 Gc = Go/(1+Go);
 S33=stepinfo(Gc)
+figure()
 bode(Go)
-[Gm, Pm, Wbredd, Wcross] = margin(Go)
+[FGm, FPm, FWbredd, FWcross] = margin(Go)
 % [gf, fsa, qwe, mkg] = margin(system)
 % Gc = feedback(K*Flead*G*Flag,1)
 % figure(5)
-% step(Gc)
+figure()
+step(Gc)
+
